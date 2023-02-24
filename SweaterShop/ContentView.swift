2a383d988f2f14cd053af86347213e758f837c3a
aselka27 <asjumalieva@gmail.com>
2023-02-24 12:13:13 +0600

    Initial Commit

---
+++ SweaterShop/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SweaterShop
+//
+//  Created by саргашкаева on 24.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SweaterShop/SweaterShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopApp.swift
+//  SweaterShop
+//
+//  Created by саргашкаева on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
